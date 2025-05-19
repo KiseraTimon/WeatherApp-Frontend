76b7cbc531e811da876fdb3b895a7a3236055740
KiseraTimon <timonmarick@gmail.com>
2025-05-19 14:08:26 +0300

    #26 - Styled Forecast Cards

--- src/Components/Cards/ForecastCard.tsx
+++ src/Components/Cards/ForecastCard.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { GetWeatherIcon } from "@/assets/GetWeatherIcon";
-import { Calendar, Thermometer } from "lucide-react";
 import { JSX } from "react";
 
 // Type Definiton
@@ -17,22 +16,39 @@ export default function ForecastCard(props: ForecastCardProps): JSX.Element {
   // Rendering Forecast Card
   return (
     <div className="card card-compact bg-base-100 shadow-md">
+      {/* 
+        <div className="card card-compact bg-base-100 shadow-md justify-start">
+        <div className="justify-start">
+          <h3 className="card-title text-lg m-mt-50 text-white-500 text-purple-500">
+            Wind
+          </h3>
+        </div>
+      
+        <p className="flex items-center space-x-10">
+          <Compass className="h-4 w-4 text-yellow-500" />
+          <span className="text-pink-500">
+            <strong className="text-yellow-500">Direction:</strong>
+            <br />
+            {dir} ({deg}°)
+          </span>
+        </p>
+        
+        */}
+
       {/* Date*/}
-      <div className="card-header flex items-center space-x-2">
-        <Calendar className="h-5 w-5 text-primary" />
-        <h3 className="card-title text-sm">{date}</h3>
+      <div className="card card-compact bg-base-100 shadow-md justify-start flex">
+        <h3 className="card-title text-sm text-orange-500">{date}</h3>
       </div>
 
       {/* Icon */}
       <figure className="h-20 flex items-center justify-center">
         {GetWeatherIcon(weatherId, {
-          className: "h-12 w-12 text-primary",
+          className: "h-12 w-12 text-orange-500",
         })}
       </figure>
 
       {/* Temp Footer */}
-      <div className="card-footer justify-center">
-        <Thermometer className="h-5 w-5 text-primary" />
+      <div className="card card-compact bg-base-100 shadow-md justify-start flex text-orange-500">
         <span className="text-lg font-medium">{temp.toFixed(1)}°C</span>
       </div>
     </div>
