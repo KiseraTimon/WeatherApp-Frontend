4e29a637b6d57e4b01605c97ab55f8a606c7c6c0
KiseraTimon <timonmarick@gmail.com>
2025-05-19 16:17:30 +0300

    #27 - Code Cleanup

--- src/Components/Cards/ForecastCard.tsx
+++ src/Components/Cards/ForecastCard.tsx
@@ -16,25 +16,6 @@ export default function ForecastCard(props: ForecastCardProps): JSX.Element {
   // Rendering Forecast Card
   return (
     <div className="card card-compact bg-base-100 shadow-md">
-      {/* 
-        <div className="card card-compact bg-base-100 shadow-md justify-start">
-        <div className="justify-start">
-          <h3 className="card-title text-lg m-mt-50 text-white-500 text-purple-500">
-            Wind
-          </h3>
-        </div>
-      
-        <p className="flex items-center space-x-10">
-          <Compass className="h-4 w-4 text-yellow-500" />
-          <span className="text-pink-500">
-            <strong className="text-yellow-500">Direction:</strong>
-            <br />
-            {dir} ({deg}°)
-          </span>
-        </p>
-        
-        */}
-
       {/* Date*/}
       <div className="card card-compact bg-base-100 shadow-md justify-start flex">
         <h3 className="card-title text-sm text-orange-500">{date}</h3>
